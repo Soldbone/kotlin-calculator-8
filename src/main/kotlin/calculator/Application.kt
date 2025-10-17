fdb82923086db519a66025bfc6cf9df1d07e4752
Easyseob <1472ezs@gmail.com>
2025-10-18 01:41:32 +0900

    feat: add split and sum feature with default delimeters

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -8,8 +8,14 @@ fun main() {
     // [feat] 기능 0: 입력
     val calculatorInput: String = Console.readLine()
 
+    // [feat] 기능 1-1: 구분자(쉼표, 콜론)를 기준으로 숫자로 분리하는 기능
+    val splitInput: List<String> = calculatorInput.split(Regex("[;,]"))
+
+    // [feat] 기능 1-2: (Built-in) 분리된 숫자들의 합을 반환하는 기능
+    val sumOfSplitInput = splitInput.sumOf { it.toInt() }
+
     // [feat] 기능 0: 출력
-    val calculatorOutput: String = calculatorInput     // temporary code line for output
+    val calculatorOutput: Int = sumOfSplitInput
     println("결과 : $calculatorOutput")
 
 }
