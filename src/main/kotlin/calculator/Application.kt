d800e88b11bcce234335d5af8b9bc548ab0a0273
Easyseob <1472ezs@gmail.com>
2025-10-18 01:24:23 +0900

    feat: add input, output feature

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,15 @@
 package calculator
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
     // TODO: 프로그램 구현
+
+    // [feat] 기능 0: 입력
+    val calculatorInput: String = Console.readLine()
+
+    // [feat] 기능 0: 출력
+    val calculatorOutput: String = calculatorInput     // temporary code line for output
+    println("결과 : $calculatorOutput")
+
 }
