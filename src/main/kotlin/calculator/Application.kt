a029fe384ef584a53cb524992c9dab37a3b11359
Easyseob <1472ezs@gmail.com>
2025-10-18 12:42:12 +0900

    fix: correct default delimeter
    Correct semicolon to colon in default delimeters(feature 1-1)

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -9,7 +9,7 @@ fun main() {
     val calculatorInput: String = Console.readLine()
 
     // [feat] 기능 1-1: 구분자(쉼표, 콜론)를 기준으로 숫자로 분리하는 기능
-    val splitInput: List<String> = calculatorInput.split(Regex("[;,]"))
+    val splitInput: List<String> = calculatorInput.split(Regex("[:,]"))
 
     // [feat] 기능 1-2: (Built-in) 분리된 숫자들의 합을 반환하는 기능
     val sumOfSplitInput = splitInput.sumOf { it.toInt() }
